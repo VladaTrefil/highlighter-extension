660009ed2e30b1d2abee18f16b02fe5f96df9db9
Vlada <vlada.trefil@gmail.com>
2020-11-19 21:14:09 +0100

    Keyboard binding highlight text command

--- background.js
+++ background.js
@@ -4,3 +4,11 @@ chrome.runtime.onMessage.addListener((request, sender) => {
     console.log(request)
   }
 })
+
+chrome.commands.onCommand.addListener((command) => {
+  if (command === 'highlightText') {
+    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+      chrome.tabs.sendMessage(tabs[0].id, { key: 'getSelection' })
+    })
+  }
+})
