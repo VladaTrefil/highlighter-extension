af5c8e8a57fbc31b48a0ad847bd714a80b4fca08
Vlada <vlada.trefil@gmail.com>
2020-11-20 14:20:32 +0100

    domSelectionOutput done + comments

--- content/domSelectionOutput.js
+++ content/domSelectionOutput.js
@@ -1,3 +1,4 @@
+// Get the common parent of anchor and focus node
 const getCommonParent = (anchorNode, focusNode) => {
   anchorNode = anchorNode.nodeType === Node.TEXT_NODE ? anchorNode.parentNode : anchorNode
   focusNode = focusNode.nodeType === Node.TEXT_NODE ? focusNode.parentNode : focusNode
@@ -11,25 +12,26 @@ const getCommonParent = (anchorNode, focusNode) => {
   return parentNode
 }
 
+// Get the text offset of start and end text node
 const getOffset = (selection, isAnchorStart) => ({
   start: isAnchorStart ? selection.anchorOffset : selection.focusOffset,
   end: !isAnchorStart ? selection.anchorOffset : selection.focusOffset,
 })
 
+// Get the start and end node from anchor and focus
 const getBoundaries = (bounds, rootNodes) => {
   const start = { index: null, node: null }
   const end = { index: null, node: null }
-  let endIndex = null
 
   rootNodes.forEach((node, index) => {
     bounds.forEach((boundary) => {
       if (node.contains(boundary)) {
         if (start.index === null) {
           start.index = index
-          start.element = boundary
+          start.node = boundary
         } else {
           end.index = index
-          end.element = boundary
+          end.node = boundary
         }
       }
     })
@@ -38,6 +40,7 @@ const getBoundaries = (bounds, rootNodes) => {
   return { start, end }
 }
 
+// Get root nodes that are between start and end node
 const getRootNodesInBounds = (startIndex, endIndex, rootNodes) => {
   const range = rootNodes.filter((node, index) => {
     if (index >= startIndex && index <= endIndex) {
@@ -50,27 +53,7 @@ const getRootNodesInBounds = (startIndex, endIndex, rootNodes) => {
   return range
 }
 
-const getSelectedNodes = (nodes, fullText, offset) => {
-  return nodes.filter((node, index) => {
-    const nodeText = node.textContent
-    let partText = ''
-
-    if (index === 0) {
-      partText = nodeText.slice(offset.start)
-    } else if (index === nodes.length) {
-      partText = nodeText.slice(0, offset.end)
-    } else {
-      partText = nodeText
-    }
-
-    if (fullText.includes(partText)) {
-      return { node, text: partText }
-    } else {
-      return false
-    }
-  })
-}
-
+// Get all text nodes from root nodes
 const getTextNodes = (nodes) => {
   const selectedNodes = []
 
@@ -86,60 +69,72 @@ const getTextNodes = (nodes) => {
   return selectedNodes
 }
 
-const getMatchingNodes = (nodes, fullText, offset) => {
+// Remove text nodes that are not between start and end node
+const removeNodesOutsideBounds = (nodes, startNode, endNode) => {
+  const startIndex = nodes.indexOf(startNode)
+  const endIndex = nodes.indexOf(endNode)
+
   return nodes.filter((node, index) => {
-    const nodeText = node.textContent
-    let partText = ''
-
-    if (index === 0) {
-      partText = nodeText.slice(offset.start)
-    } else if (index === nodes.length) {
-      partText = nodeText.slice(0, offset.end)
-    } else {
-      partText = nodeText
-    }
-
-    if (fullText.includes(partText)) {
-      return { node, text: partText }
+    if (index >= startIndex && index <= endIndex) {
+      return node
     } else {
       return false
     }
   })
 }
 
-const parseSelectedNodes = () => {
-  let text = ''
+// Get text nodes which text content matches text selection
+const getMatchingNodes = (nodes, fullText, offset) => {
+  return nodes
+    .map((node, index) => {
+      const nodeText = node.textContent
+      let partText = ''
 
-  if (window.getSelection) {
-    const selection = window.getSelection()
+      if (index === 0) {
+        partText = nodeText.slice(offset.start)
+      } else if (index === nodes.length - 1) {
+        partText = nodeText.slice(0, offset.end)
+        console.log(nodeText, offset.end)
+      } else {
+        partText = nodeText
+      }
 
-    text = selection.toString()
-
-    const anchorNode = selection.anchorNode
-    const focusNode = selection.focusNode
-
-    const parentNode = getCommonParent(anchorNode, focusNode)
-    const rootNodes = Object.values(parentNode.childNodes)
-    const { start, end } = getBoundaries([anchorNode, focusNode], rootNodes)
-
-    const selectedRootNodes = getRootNodesInBounds(start.index, end.index, rootNodes)
-
-    if (rootNodes.length > 0) {
-      const isAnchorStart = rootNodes[0].contains(anchorNode)
-      const offset = getOffset(selection, isAnchorStart)
-
-      const textNodes = getTextNodes(selectedRootNodes)
-      const textNodes = getTextNodes(selectedNodes)
-
-      const matchingNodes = getMatchingNodes(textNodes, text, offset)
-
-      return matchingNodes
-    }
-  }
-
-  return false
+      if (fullText.includes(partText)) {
+        return { node, text: partText }
+      } else {
+        return false
+      }
+    })
+    .filter((node) => node)
 }
 
+// Get output from selection in selected nodes and text
 const getDomSelectionOutput = () => {
-  return parseSelectedNodes()
+  if (!window.getSelection) {
+    return
+  }
+
+  const selection = window.getSelection()
+
+  const text = selection.toString()
+
+  const anchorNode = selection.anchorNode
+  const focusNode = selection.focusNode
+
+  const parentNode = getCommonParent(anchorNode, focusNode)
+  const rootNodes = Object.values(parentNode.childNodes)
+  const { start, end } = getBoundaries([anchorNode, focusNode], rootNodes)
+
+  const selectedRootNodes = getRootNodesInBounds(start.index, end.index, rootNodes)
+
+  if (rootNodes.length > 0) {
+    const isAnchorStart = rootNodes[0].contains(anchorNode)
+    const offset = getOffset(selection, isAnchorStart)
+
+    const textNodes = getTextNodes(selectedRootNodes)
+    const selectedNodes = removeNodesOutsideBounds(textNodes, start.node, end.node)
+    const matchingNodes = getMatchingNodes(selectedNodes, text, offset)
+
+    return { nodes: matchingNodes, text, parent: parentNode }
+  }
 }
--- content/messageHandler.js
+++ content/messageHandler.js
@@ -1,6 +1,8 @@
 chrome.runtime.onMessage.addListener((request, sender) => {
   if (request.key === 'getSelection') {
-    const selection = parseSelectedNodes()
+    const selection = getDomSelectionOutput()
+
+    console.log(selection)
 
     chrome.runtime.sendMessage({ key: 'setSelection', selection: JSON.stringify(selection) })
   }
